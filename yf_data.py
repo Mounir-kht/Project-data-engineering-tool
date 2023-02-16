e732c6f3a79908e3c3b69d4ede576f1d2884bc16
Mounir-kht <mounirkhettou@gmail.com>
2023-02-16 17:21:35 +0100

    final version

--- yf_data.py
+++ yf_data.py
@@ -1,13 +1,12 @@
 import yfinance as yf
 from pymongo import MongoClient
-import pandas as pd
 def yf_data(quote = 'AAPL', period = '1mo', interval = '1d'):
-    quote = yf.Ticker('quote')
+    quote = yf.Ticker(quote)
     hist = quote.history(period=period, interval=interval)
     hist.drop(['Dividends','Stock Splits'], axis = 1, inplace = True)
     
     client = MongoClient('mongo')
     db = client["reddit_db"]
-    yf = db["yahoo_finance"]
-    yf.insert_many(hist.to_dict('records'))
+    yf_db = db["yahoo_finance"]
+    yf_db.insert_many(hist.to_dict('records'))
     return 
